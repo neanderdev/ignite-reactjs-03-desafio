4555a53ee525be59cf343432df7977ab767de697
Neander de Souza <neander.faria@hotmail.com>
2023-06-26 15:55:27 -0300

    Configurando rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+import { Router } from './Router';
 
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/theme/default';
@@ -7,6 +10,10 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,10 +6,12 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
   body {
     color: ${(props) => props.theme["text"]};
     background: ${(props) => props.theme["background"]};
   }
+  
   body, input {
     font-family: 'Nunito', sans-serif;
     line-height: 160%;

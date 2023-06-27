5cbb4946a743d24c3deb7eeada3bda280fce055e
Neander de Souza <neander.faria@hotmail.com>
2023-06-27 09:58:45 -0300

    Componente: Repository

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,15 @@ import { DefaultLayout } from './layouts/DefaultLayout';
 
 import { Blog } from './pages/Blog';
 import { Post } from './pages/Post';
+import { Repository } from './pages/Repository';
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={<DefaultLayout />}>
                 <Route path="/" element={<Blog />} />
+
+                <Route path="/repository" element={<Repository />} />
 
                 <Route path="/post">
                     <Route path=":issueId" element={<Post />} />
